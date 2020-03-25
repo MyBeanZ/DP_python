b73b2ac603f6bacd67e4c5c82481e82d6d4e6944
MyBeanZ <60071900+MyBeanZ@users.noreply.github.com>
2020-03-25 13:50:30 +0100

    Main window complete

--- SUM_X_Y.py
+++ SUM_X_Y.py
@@ -16,7 +16,7 @@ import random
 import matplotlib
 # Make sure that we are using QT5
 matplotlib.use('Qt5Agg')  #Agg je renderer - vykreslujici process - moznosti WXAgg, GTKAgg, QT4Agg
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtWidgets import QWidget, QTableWidget, QTableWidgetItem
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -125,7 +125,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         #self.resize(900, 880)
-        self.setGeometry(300, 150, 900, 860)
+        self.setGeometry(300, 130, 900, 860)
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
         self.setWindowTitle("PSD - test")
 
@@ -148,7 +148,12 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
 
-        """ graf quad  """
+        """-----others -----------"""
+        self.centralwidget.setFocus()  # pohled na central widget - Qapp velka plocha
+        self.setCentralWidget(self.centralwidget)
+        self.statusBar().showMessage("Welcome", 3000)
+
+        """ graf quad  -------------"""
         self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
         self.verticalLayoutWidget.setGeometry(QtCore.QRect(30, 40, 500, 400))
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
@@ -160,7 +165,9 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.layout_plot.setContentsMargins(0, 0, 0, 0)
         self.layout_plot.setObjectName("layout_plot")
         self.layout_plot.addWidget(self.quad)
-        """graf X v case - LAYOUT """
+        """graf X v case - LAYOUT -----------------"""
+        font_time = QtGui.QFont()
+        font_time.setPointSize(9)
         self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
         self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(30, 500, 400, 300))
         self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
@@ -170,13 +177,14 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
         self.label_X = QtWidgets.QLabel(self.verticalLayoutWidget_2)  # self pred label kvuli pristupu z cele tridy
         self.label_X.setText("X samples in time")
+        self.label_X.setFont(font_time)
         self.label_X.setGeometry(0, 15, 130, 18)
 
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
         self.verticalLayout_2.setContentsMargins(0, 50, 0, 0)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.verticalLayout_2.addWidget(self.x_time)
-        """graf Y v case - LAYOUT """
+        """graf Y v case - LAYOUT --------------------"""
         self.verticalLayoutWidget_3 = QtWidgets.QWidget(self.centralwidget)
         self.verticalLayoutWidget_3.setGeometry(QtCore.QRect(450, 500, 400, 300))
         self.verticalLayoutWidget_3.setObjectName("verticalLayoutWidget_3")
@@ -186,6 +194,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
         self.label_Y = QtWidgets.QLabel(self.verticalLayoutWidget_3)  # self pred label kvuli pristupu z cele tridy
         self.label_Y.setText("Y samples in time")
+        self.label_Y.setFont(font_time)
         self.label_Y.setGeometry(0, 15, 130, 20)
 
         self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_3)
@@ -193,26 +202,67 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.verticalLayout_3.setObjectName("verticalLayout_3")
         self.verticalLayout_3.addWidget(self.y_time)
 
-        """tlacitko STart/STOP"""
+        """tlacitko STart/STOP --------- tlacitko CONNECT """
         self.butt_start = QtWidgets.QPushButton(self.centralwidget)
-        self.butt_start.setGeometry(QtCore.QRect(585, 440, 93, 28))
+        self.butt_start.setGeometry(QtCore.QRect(585, 410, 93, 28))
         self.butt_start.setCheckable(True)
         self.butt_start.setChecked(False)  #pocatecni stav STOP => setChecked(False)
         self.butt_start.setEnabled(False)
         self.butt_start.setObjectName("pushButton")
         self.butt_start.clicked.connect(self.clicked)
         self.butt_start.setText("Start")
-        """tlacitko CONNECT """
+
         self.butt_conn = QtWidgets.QPushButton(self.centralwidget)
-        self.butt_conn.setGeometry(QtCore.QRect(685, 440, 93, 28))
+        self.butt_conn.setGeometry(QtCore.QRect(685, 410, 93, 28))
         self.butt_conn.setEnabled(True)
+        self.butt_conn.setCheckable(True)
+        self.butt_conn.setChecked(False)
         self.butt_conn.setText("Connect")
         self.butt_conn.clicked.connect(self.connect)
 
-        self.centralwidget.setFocus()
-        self.setCentralWidget(self.centralwidget)
+        """ --------- connection labels ----------"""
+        font_con = QtGui.QFont()
+        font_con.setPointSize(9)
 
-        self.statusBar().showMessage("Welcome", 2000)
+        self.label_con = QtWidgets.QLabel(self.centralwidget)  # self pred label kvuli pristupu z cele tridy
+        self.label_con.setFont(font_con)
+        self.label_con.setText("Connection: ")
+        self.label_con.setGeometry(585, 450, 100, 20)
+
+        self.lab_stat = QtWidgets.QLabel(self.centralwidget)  # self pred label kvuli pristupu z cele tridy
+        self.lab_stat.setFont(font_con)
+        self.lab_stat.setStyleSheet('color: red')
+        self.lab_stat.setText("Not connected")
+        self.lab_stat.setGeometry(685, 450, 300, 20)
+
+        """------- line & label Time ------------30, 500, 400, 300"""
+        time_line_coor = [100,480]
+        self.line_time = QtWidgets.QFrame(self.centralwidget)
+        self.line_time.setGeometry(QtCore.QRect(time_line_coor[0], time_line_coor[1], 730, 20))
+        self.line_time.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line_time.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_time.setObjectName("line_time")
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label_time = QtWidgets.QLabel(self.centralwidget)  # self pred label kvuli pristupu z cele tridy
+        self.label_time.setFont(font)
+        self.label_time.setText("Time ")
+        self.label_time.setGeometry(time_line_coor[0] - 60, time_line_coor[1], 70, 20)
+
+        """------- line & label X - Y ------------30, 500, 400, 300"""
+        x_y_line_coor = [100, 10]
+        self.line_x_y = QtWidgets.QFrame(self.centralwidget)
+        self.line_x_y.setGeometry(QtCore.QRect(x_y_line_coor[0], x_y_line_coor[1], 730, 20))
+        self.line_x_y.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line_x_y.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_x_y.setObjectName("line_time")
+        font_x_y = QtGui.QFont()
+        font_x_y.setPointSize(10)
+        self.label_x_y = QtWidgets.QLabel(self.centralwidget)  # self pred label kvuli pristupu z cele tridy
+        self.label_x_y.setFont(font_x_y)
+        self.label_x_y.setText("X - Y ")
+        self.label_x_y.setGeometry(x_y_line_coor[0]- 60, x_y_line_coor[1], 70, 20)
+
         """ ----- DATA X Y prompt (table)  ------"""
         self.tableWidget = QTableWidget(self.centralwidget)
         self.tableWidget.setRowCount(self.quad.d_len)
@@ -227,7 +277,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.timer.timeout.connect(self.read_data)
         #self.timer.start(self.s_time)
 
-    """ostatni fce"""
+    """-----------------------------------FUNKCE/METODY TRID GUI ----------------------------------------------------"""
     def read_data(self): # Tato fce bezi nepretrzite - z duvodu kontroli pripojeni
 
         try:
@@ -259,6 +309,8 @@ class ApplicationWindow(QtWidgets.QMainWindow):
             self.quad.Y_data = y_div
         except:
             self.statusBar().showMessage("Unable to read - Disconnected", 3000)
+            self.lab_stat.setText("Disconnected")
+            self.lab_stat.setStyleSheet('color: red')
             self.butt_conn.setEnabled(True)
             self.butt_start.setEnabled(False)
             self.butt_start.setChecked(False)
@@ -275,7 +327,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
 
 
-
+    """-----------------start/stop fce -----------------"""
     def clicked(self):
         cond = self.butt_start.isChecked()
         if not (cond):
@@ -292,24 +344,39 @@ class ApplicationWindow(QtWidgets.QMainWindow):
             self.y_time.timer.start(self.s_time)
             self.statusBar().showMessage("Started", 2000)
 
-
+    """-----------------------connect ---------------------"""
     def connect (self):
-        try:
-            """ ----- pripojeni serioveho portu ----------"""
-            self.s = serial.Serial(self.port_name, baudrate=115200, timeout=None, bytesize=8, parity='N', rtscts=0)
-            print(self.s.name)
-            self.timer.start(self.s_time)
-            stat_con = "Connected to: " + self.s.name
-            self.statusBar().showMessage(self.s.name, 3000)
-            self.butt_start.setEnabled(True)
-            self.butt_conn.setEnabled(False)
-            self.statusBar().showMessage(stat_con, 2000)
-        except:
-            self.statusBar().showMessage("Unable to connect", 2000)
+        cond = self.butt_conn.isChecked()
+        if cond:
+            try:
+                """ ----- pripojeni serioveho portu ----------"""
+                self.s = serial.Serial(self.port_name, baudrate=115200, timeout=None, bytesize=8, parity='N', rtscts=0)
+                print(self.s.name)
+                self.timer.start(self.s_time)
+                stat_con = "Connected to: " + self.s.name
+                self.statusBar().showMessage(self.s.name, 3000)
+                self.butt_start.setEnabled(True)
+                #self.butt_conn.setChecked(True)
+                self.butt_conn.setText("Disconnect")
+                self.lab_stat.setText(str(self.s.name))
+                self.lab_stat.setStyleSheet('color: green')
+                self.statusBar().showMessage(stat_con, 2000)
+            except:
+                self.statusBar().showMessage("Unable to connect", 2000)
+                self.butt_conn.setChecked(False)
+                string = "Unable to connect to: " + self.port_name
+                self.lab_stat.setText(string)
+                self.lab_stat.setStyleSheet('color: orange')
+        else:
+            self.s.close()
+            self.statusBar().showMessage("Disconnected", 3000)
+            self.butt_start.setEnabled(False)
+            self.butt_conn.setText("Connect")
 
 
 
-    """  funkce z Menu"""
+
+    """  ------------------ funkce z Menu --------------------"""
     def fileQuit(self):
         self.s.close()
         self.close()
